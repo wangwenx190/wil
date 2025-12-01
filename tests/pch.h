e75d9fa486f513822c9c521810147eae75ae3b2b
Zhao Yuhang <2546789017@qq.com>
2025-12-01 19:10:57 +0800

    Custom tweaks

--- tests/pch.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-// Suppress 'noreturn' attributes from causing issues
-#define RESULT_NORETURN
-#define RESULT_NORETURN_NULL
-#define RESULT_NORETURN_RESULT(expr) return (expr)
-
-// Definition using '__declspec(selectany)' not compatible with -fno-ms-compatibility
-#define XMGLOBALCONST inline constexpr const
